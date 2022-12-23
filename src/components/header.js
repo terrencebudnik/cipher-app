72ab47c188229944757c70882edf2a1b2a24ff3b
terrence <terrence.budnik@gmail.com>
2022-12-22 20:07:03 -0600

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+const characters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890*#@/*!%&^"];
+
+
+function Header() {
+    return (
+        <div>
+
+        </div>
+    )}
+  
+  export default Header;
+  
