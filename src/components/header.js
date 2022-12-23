cf91b63fc1098cc1fec99978498d0fa117ac4f4e
terrence <terrence.budnik@gmail.com>
2022-12-22 20:55:13 -0600

    added cipher effect

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,16 @@
-const characters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890*#@/*!%&^"];
+import headerMod from '../modules/headerMod';
+import '../css/header.css'
 
 
 function Header() {
+    
+    headerMod();
+    
     return (
         <div>
-
+            <a href="#" class="cipher">Welcome to Cipher</a>
         </div>
-    )}
-  
-  export default Header;
-  
+    )
+}
+
+export default Header;
